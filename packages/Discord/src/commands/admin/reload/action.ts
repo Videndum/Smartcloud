65b03c26833f8d7d98c6c3746b18c537e2063af4
Jonathan Stevens <jonathan@tgtmedia.org>
2020-02-23 13:12:52 +0000

    refactor: command

--- packages/Discord/src/commands/admin/reload/action.ts
+++ packages/Discord/src/commands/admin/reload/action.ts
@@ -20,33 +20,33 @@ module.exports = {
     helpInformation: "",
     execute: (root:any, message:any, args:any) => new Promise<string>((resolve, reject) => {
         root.log(`${message.author.username} Global Access: ${root.classes.user.checkUserRank(message.author.id)}`, 3);
-        if (!root.classes.user.checkUserRank(message.author.id)) {
-            embeds.admin.accessFailed(message, root);
-            reject({err:"accessfailed", data: "User Rank was not sufficient"});
-        }
-        else {
-          if (!args[0]) embeds.admin.help(message, root);
-          switch (args[0]){
-            case "reload":
-              if (!args[1]) embeds.admin.noreload(message, root);
-              switch (args[1]){
-                case "command" || "commands":
-                  const cmd = root.commands[args[2]];
-                  console.log("reloading command ", cmd)
-                break;
-                case "action" || "actions":
-                  const action = root.actions[args[2]];
-                  console.log("reloading command ", action)
-                break;
-                case "event" || "events":
-                  // const event = root.eventlist[args[2]];
-                  // console.log("reloading command ", event)
-                break;
-              }
-              break;
-          }
-          resolve();
-        }
+        // if (!root.classes.user.checkUserRank(message.author.id)) {
+        //     embeds.admin.accessFailed(message, root);
+        //     reject({err:"accessfailed", data: "User Rank was not sufficient"});
+        // }
+        // else {
+        //   if (!args[0]) embeds.admin.help(message, root);
+        //   switch (args[0]){
+        //     case "reload":
+        //       if (!args[1]) embeds.admin.noreload(message, root);
+        //       switch (args[1]){
+        //         case "command" || "commands":
+        //           const cmd = root.commands[args[2]];
+        //           console.log("reloading command ", cmd)
+        //         break;
+        //         case "action" || "actions":
+        //           const action = root.actions[args[2]];
+        //           console.log("reloading command ", action)
+        //         break;
+        //         case "event" || "events":
+        //           // const event = root.eventlist[args[2]];
+        //           // console.log("reloading command ", event)
+        //         break;
+        //       }
+        //       break;
+          // }
+        // }
+        resolve();
     })
 };
 //process.uptime() --> uptime
