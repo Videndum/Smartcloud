6be88dab1b75181995735f4b0125e063c8964300
Jonathan Stevens <jonathan@tgtmedia.org>
2020-02-24 04:55:27 +0000

    feat: add prompts

---
+++ packages/Hub/modules/default/prompts/commands.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function doesntExist(message) {
+    message.reply(`That command does not exist, please try again.`);
+}
+exports.doesntExist = doesntExist;
+function cooldown(message, timeLeft, cmd) {
+    message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${cmd.name}\` command.`);
+}
+exports.cooldown = cooldown;
+function dms(message) {
+    message.reply('I can\'t execute that command inside DMs!');
+}
+exports.dms = dms;
+function args(message, prefix, cmd) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+    if (cmd.usage) {
+        reply += `\nThe proper usage would be: \`${prefix}${cmd.name} ${cmd.usage}\``;
+    }
+    message.channel.send(reply);
+}
+exports.args = args;
---
+++ packages/Hub/modules/default/prompts/commands.ts
@@ -0,0 +1,16 @@
+export function doesntExist (message:any) {
+  message.reply(`That command does not exist, please try again.`);
+}
+export function cooldown (message:any, timeLeft:any, cmd:any) {
+  message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${cmd.name}\` command.`);
+}
+export function dms (message:any) {
+  message.reply('I can\'t execute that command inside DMs!');
+}
+export function args (message:any, prefix:any, cmd:any) {
+  let reply = `You didn't provide any arguments, ${message.author}!`;
+  if (cmd.usage) {
+    reply += `\nThe proper usage would be: \`${prefix}${cmd.name} ${cmd.usage}\``;
+  }
+  message.channel.send(reply);
+}
