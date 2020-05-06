99529114f82055425fa08c71c67ebb5bfcff7ef3
Jonathan S <sjonathan@hotmail.co.uk>
2020-05-06 16:08:46 +0100

    Chore: Formatting Improvements + package updates
    Signed-off-by: Jonathan S <sjonathan@hotmail.co.uk>

--- packages/Hub/src/index.ts
+++ packages/Hub/src/index.ts
@@ -1,6 +1,6 @@
 export const core = require("@smartcloudai/core").core;
 import { Worker } from 'worker_threads';
-function log (data: string, level: number | undefined) {core.log(data,level)}
+function log(data: string, level: number | undefined) { core.log(data, level) }
 // const applications: any[] = []
 import { build } from './classes/extensions'
 import { api } from './api'
@@ -9,16 +9,16 @@ import { api } from './api'
 * @private
 */
 async function initialize() {
-  core.pm.connect(function(err: any) {
-    if (err) { console.error(err); process.exit(2);}
+  core.pm.connect(function (err: any) {
+    if (err) { console.error(err); process.exit(2); }
   });
-  console.clear()
+  // console.clear()
   log("Starting Application", 1)
   /**
   * Sentry initialization to track errors
   */
   if (core.config.tracking == true) {
-   core.log('Sentry initialize to track errors and bugs. You can turn off in your settings',2)
+    core.log('Sentry initialize to track errors and bugs. You can turn off in your settings', 2)
     try {
       core.sentry.init({
         dsn: core.config.sentry,
@@ -28,38 +28,38 @@ async function initialize() {
         serverName: process.env.PM2_MACHINE_NAME,
         attachStacktrace: true
       });
-      core.sentry.configureScope((scope:any) => {
-        scope.setUser({"email": core.config.license_email});
+      core.sentry.configureScope((scope: any) => {
+        scope.setUser({ "email": core.config.license_email });
         scope.setTag("page_locale", "en-GB");
       });
-    } catch(_){
-      core.log("Sentry Error: " +_)
+    } catch (_) {
+      core.log("Sentry Error: " + _)
     }
   }
-  log('Smart cloud service started',2);
-  log(' ',2);
-  log(' ',2);
-  log(' ',2);
-  log(' ',2);
-  log(' ',2);
-  log(' ',2);
-  log(' ',2);
-  log(' ',2);
-  log('      .:: ::                                  .::           .::    .::                       .::',2);
-  log('    .::    .::                                .::        .::   .:: .::                       .::',2);
-  log('     .::      .::: .:: .::    .::    .: .:::.:.: .:     .::        .::   .::    .::  .::     .::',2);
-  log('       .::     .::  .:  .:: .::  .::  .::     .::       .::        .:: .::  .:: .::  .:: .:: .::',2);
-  log('          .::  .::  .:  .::.::   .::  .::     .::       .::        .::.::    .::.::  .::.:   .::',2);
-  log('    .::    .:: .::  .:  .::.::   .::  .::     .::        .::   .:: .:: .::  .:: .::  .::.:   .::',2);
-  log('      .:: ::  .:::  .:  .::  .:: .:::.:::      .::         .::::  .:::   .::      .::.:: .:: .::',2);
-  log('                                                                                                ',2);
-  log('                                                                                                ',2);
-  log('                               Smart Cloud Core Module Launched',2);
-  log('                                 Developed by TGT Media Ltd',2);
-  log('                                     info@tgtmedia.org',2);
-  log('                                        tgtmedia.org',2);
-  log(' ',2);
-  log(' ',2);
+  log('Smart cloud service started', 2);
+  log(' ', 2);
+  log(' ', 2);
+  log(' ', 2);
+  log(' ', 2);
+  log(' ', 2);
+  log(' ', 2);
+  log(' ', 2);
+  log(' ', 2);
+  log('      .:: ::                                  .::           .::    .::                       .::', 2);
+  log('    .::    .::                                .::        .::   .:: .::                       .::', 2);
+  log('     .::      .::: .:: .::    .::    .: .:::.:.: .:     .::        .::   .::    .::  .::     .::', 2);
+  log('       .::     .::  .:  .:: .::  .::  .::     .::       .::        .:: .::  .:: .::  .:: .:: .::', 2);
+  log('          .::  .::  .:  .::.::   .::  .::     .::       .::        .::.::    .::.::  .::.:   .::', 2);
+  log('    .::    .:: .::  .:  .::.::   .::  .::     .::        .::   .:: .:: .::  .:: .::  .::.:   .::', 2);
+  log('      .:: ::  .:::  .:  .::  .:: .:::.:::      .::         .::::  .:::   .::      .::.:: .:: .::', 2);
+  log('                                                                                                ', 2);
+  log('                                                                                                ', 2);
+  log('                               Smart Cloud Core Module Launched', 2);
+  log('                                 Developed by TGT Media Ltd', 2);
+  log('                                     info@tgtmedia.org', 2);
+  log('                                        tgtmedia.org', 2);
+  log(' ', 2);
+  log(' ', 2);
   if (core.package.version.split(".")[2].split("-")[1]) log(`Running version development version ${core.package.version.split(".")[2].split("-")[1]}. Please be careful when running an unstable version`, 3)
   log('Log file: ' + core.appender.__protected().currentFile, 2); //Log which file is current log file
   log("Log Level: " + core.loglevels[core.config.logLevel], 2)
@@ -83,7 +83,7 @@ async function initialize() {
 //   api: false
 // }
 
-function runService(service:string, workerData: any) {
+function runService(service: string, workerData: any) {
   return new Promise((resolve, reject) => {
     const worker = new Worker(service, { workerData });
     worker.on('message', resolve);
